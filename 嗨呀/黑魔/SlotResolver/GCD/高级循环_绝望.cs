78fce018b22a5fa5cc9a926a26ac8f212882fca5
嗨呀www <991876906@qq.com>
2026-06-20 04:25:12 +0800

    Update 高级循环_绝望.cs

--- 嗨呀/黑魔/SlotResolver/GCD/高级循环_绝望.cs
+++ 嗨呀/黑魔/SlotResolver/GCD/高级循环_绝望.cs
@@ -12,7 +12,7 @@ public class 高级循环_绝望 : ISlotResolver
         if (!BLMHelper.火状态) return (int)CheckResult.高级循环绝望_未进火;
         if (BLMHelper.火层数 >= 3) return (int)CheckResult.高级循环绝望_星灵未完成;
         if (!BLM_BattleData.Instance.高级循环_冰澈读条完成) return (int)CheckResult.高级循环绝望_前一Gcd不符;
-        if (!GameHelper.RecentlyUsedSpell(BLMHelper.冰澈, (int)GCDHelper.GetGCDDuration())) return (int)CheckResult.高级循环绝望_前一Gcd不符;
+        if (!GameHelper.RecentlyUsedSpell(BLMHelper.冰澈, (int)GCDHelper.GetGCDDuration(BLMHelper.冰澈))) return (int)CheckResult.高级循环绝望_前一Gcd不符;
         var mp = Data.Me.Object?.CurrentMp ?? 0;
         if (mp < 800) return (int)CheckResult.资源不足;
         return 0;
